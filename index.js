29e9bafc891f1f079363ca6858a020813506cc2e
Martin Moy <martinmoy1991@gmail.com>
2020-03-21 00:20:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function countdown(x) {
   for (let countdown = x; countdown >= 0; countdown--) {
 console.log(countdown);}
@@ -11,4 +12,15 @@ function writeCards(names, event) {
     );
   }
   return thankYouCards;
+=======
+// Code your solutions in this file
+const array = ["Ada", "Brendan", "Ali"];
+
+function writeCards(array).splice {
+  for (let i = 0; i <array.length; i++){
+    console.log("Thank you, ${array[i]}, for the wonderful birthday gift!");}
+
+
+
+>>>>>>> c0bba15601d4ca0c2eedfeab13a58453869ee5d5
 }
